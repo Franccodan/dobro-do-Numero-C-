b398ac5f5cb8a7bb05d033c26a0ae995b7ee4a49
Daniel Franco <82846096+Franccodan@users.noreply.github.com>
2022-03-22 21:11:22 -0300

    Add files via upload

---
+++ dobroNumero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dobroNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0;
+            int numeroDobro = 0;
+
+    
+            Console.Write("Digite um número: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            numeroDobro = numero1 * 2;
+
+            Console.Write("Olá, o dobro deste número é: " + numeroDobro);
+            Console.ReadLine();
+            
+        }
+    }
+}
+ 
